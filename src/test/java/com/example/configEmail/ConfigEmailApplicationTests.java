24ede86d422881fee0870d5db43fdee9ed709257
Namtran <namtran.210102@gmail.com>
2024-06-03 10:52:16 +0700

    first

---
+++ src/test/java/com/example/configEmail/ConfigEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
